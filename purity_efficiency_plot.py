97eb7a1b28b7e6c02404b809371bc1a84455b9e9
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2016-11-20 14:58:00 +0000

    Legend to purity ploy

--- purity_efficiency_plot.py
+++ purity_efficiency_plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.4
 
-from ROOT import TGraphErrors, kBlue, kRed, TCanvas
+from ROOT import TGraphErrors, kBlue, kRed, TCanvas, TLegend
 from numpy import array
 
 eff_list = []
@@ -22,16 +22,24 @@ c = TCanvas("c")
 g_eff = TGraphErrors(len(eff_list), array(tolerance_list), array(eff_list), no_err, array(eff_err_list))
 g_purity = TGraphErrors(len(purity_list), array(tolerance_list), array(purity_list), no_err, array(purity_err_list))
 
-#g_purity = TGraphErrors(len(purity_list), array(tolerance_list), array(purity_list))
-
 g_eff.Draw("APL")
+g_eff.GetXaxis().SetTitle("Tolerance [cm]")
+g_eff.GetYaxis().SetTitle("[%]")
+
 g_eff.SetMarkerStyle(20)
 g_eff.SetLineColor(kRed+1)
 g_eff.SetLineWidth(2)
 g_purity.Draw("PL")
-g_purity.SetMarkerStyle(20)
+g_purity.SetMarkerStyle(21)
 g_purity.SetLineColor(kBlue+1)
 g_purity.SetLineWidth(2)
+
+l = TLegend(0.66,0.16,0.89,0.27)
+l.SetBorderSize(0)
+l.SetShadowColor(0)
+l.AddEntry(g_eff,"Efficiency", "lp")
+l.AddEntry(g_purity,"Purity", "lp")
+l.Draw()
 
 c.Update()
 input()
