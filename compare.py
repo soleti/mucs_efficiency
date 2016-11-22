c20c0bd2308e9f5811627801d5a04dd8f28a9407
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2016-11-22 17:13:15 +0000

    add systematic effect

--- compare.py
+++ compare.py
@@ -40,13 +40,15 @@ from ROOT import TH1F, TCanvas, gStyle
 
 gStyle.SetOptStat(0)
 
-h_sig = TH1F("h_sig",";Significance [#sigma];N. Entries / 0.5",20,-10,10)
+h_sig = TH1F("h_sig",";Significance [#sigma];N. Entries / 1",10,-5,5)
 
 def measure_sig(val1,val2):
     return (val1-val2)/math.sqrt(err1**2+err2**2)
     
+    
+sys = 0.0831
 print("0 - Central, 1 - upstream, 2 - downstream")
-with open("central_shift.txt") as textfile1, open("upstream_shift.txt") as textfile2, open("downstream_shift.txt") as textfile3: 
+with open("output/central_shift.txt") as textfile1, open("output/upstream_shift.txt") as textfile2, open("output/downstream_shift.txt") as textfile3: 
     for x, y, z in zip(textfile1, textfile2, textfile3):
         a = x.split(" ")
         a = [float(i) for i in a]
@@ -58,24 +60,24 @@ with open("central_shift.txt") as textfile1, open("upstream_shift.txt") as textf
         eff = [a[3],b[3],c[3]]
         err = [a[4],b[4],c[4]]
         
-        if eff[0] != 0 and eff[1] != 0:
-            sig = (eff[0]-eff[1])/math.sqrt(err[0]**2+err[1]**2)
+        if eff[0] != 0 and eff[1] != 0 and eff[0] != 1 and eff[1] != 1:
+            sig = (eff[0]-eff[1])/math.sqrt(err[0]**2+err[1]**2+2*sys**2)
             h_sig.Fill(sig)
-            if abs(sig) > 4:
-                print(0,1,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig)
+            if abs(sig) > 3:
+                print("%i %i %.2f %.2f %.2f %.2f %.2f %.2f %.2f" % (0,1,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig))
         
-        if eff[0] != 0 and eff[2] != 0:
-            sig = (eff[2]-eff[0])/math.sqrt(err[0]**2+err[2]**2)
+        if eff[0] != 0 and eff[2] != 0 and eff[0] != 1 and eff[2] != 1:
+            sig = (eff[2]-eff[0])/math.sqrt(err[0]**2+err[2]**2+2*sys**2)
             h_sig.Fill(sig)
-            if abs(sig) > 4:
-                print(0,2,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig)
+            if abs(sig) > 3:
+                print("%i %i %.2f %.2f %.2f %.2f %.2f %.2f %.2f" % (0,2,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig))
                 
-        if eff[1] != 0 and eff[2] != 0:
-            sig = (eff[1]-eff[2])/math.sqrt(err[1]**2+err[2]**2)
-            h_sig.Fill(sig)
-            if abs(sig) > 4:
-                print(1,2,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig)
-
+        if eff[1] != 0 and eff[2] != 0 and eff[1] != 1 and eff[2] != 1:
+            sig = (eff[1]-eff[2])/math.sqrt(err[1]**2+err[2]**2+2*sys**2)
+            if abs(sig) > 3:
+                print("%i %i %.2f %.2f %.2f %.2f %.2f %.2f %.2f" % (1,2,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig))
+            else:
+                h_sig.Fill(sig)
 
                     
 
@@ -86,6 +88,7 @@ h_sig.Draw("ep")
 h_sig.SetMarkerStyle(20)
 h_sig.SetLineColor(1)
 h_sig.Fit("gaus")
+print(h_sig.Integral())
 #h_sig.GetYaxis().SetRangeUser(0.01,h_sig.GetMaximum()*1.2)
 c_sig.Update()
 
