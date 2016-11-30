20b0dc303a7e64f9e093e4dd24b07b2e94d98bee
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2016-11-30 12:18:17 +0000

    theta phi l dictionary

--- compare.py
+++ compare.py
@@ -45,9 +45,12 @@ h_sig = TH1F("h_sig",";Significance [#sigma];N. Entries / 1",20,-10,10)
 def measure_sig(val1,val2):
     return (val1-val2)/math.sqrt(err1**2+err2**2)
     
+theta_angle = {7: 90, 8: 105, 5: 60, 6: 75}
+phi_angle = {7: -90, 8: -75, 9: -60}
+l_dict = {1: 20, 2: 80, 3: 140, 4: 200, 5: 260}
     
 sys = 0
-sys_value = 0
+sys_value = 0.1
 print("0 - Central, 1 - upstream, 2 - downstream")
 with open("output/central_shift.txt") as textfile1, open("output/upstream_shift.txt") as textfile2, open("output/downstream_shift.txt") as textfile3: 
     for x, y, z in zip(textfile1, textfile2, textfile3):
@@ -57,34 +60,42 @@ with open("output/central_shift.txt") as textfile1, open("output/upstream_shift.
         b = [float(i) for i in b]
         c = z.split(" ")
         c = [float(i) for i in c]
-        
+        #print(a[0],b[0],c[0],a[1],b[1],c[1])
         eff = [a[3],b[3],c[3]]
         err = [a[4],b[4],c[4]]
         theta = a[0]
         phi = a[1]
+        l = a[2]
+
+        #if theta_angle[theta] == 90 and (phi_angle[phi] == -60 or phi_angle[phi] == -75):
+        #    sys = 0.14
+        #elif theta_angle[theta] == 75  and (phi_angle[phi] == -60 or phi_angle[phi] == -75):
+        #    sys = 0.07
+        #elif theta_angle[theta] == 60  and phi_angle[phi] == -60:
+        #    sys = 0.09
+        #else:
+        #    sys = 0
+        if theta == 7 or theta == 6:
+            sys = 0.03
+        #sys = 0
         if eff[0] != 0 and eff[1] != 0 and eff[0] != 1 and eff[1] != 1:
-            if (theta == 7 and phi >= 7 and phi <= 9) or (theta == 5 and phi == 9):
-                sys = sys_value
-            sig = (eff[0]-eff[1])/math.sqrt(err[0]**2+err[1]**2+2*sys**2)
-            print(err[0],err[1])
-            h_sig.Fill(sig)
+            sig = (-eff[0]+eff[1])/math.sqrt(err[0]**2+err[1]**2+2*sys**2)
             if abs(sig) > 3:
-                print("%i %i %.2f %.2f %.2f %.2f %.2f %.2f %.2f" % (0,1,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig))
+                print("%.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, theta_angle[theta], phi_angle[phi], l_dict[l]))
+            h_sig.Fill(sig)
+
         
         if eff[0] != 0 and eff[2] != 0 and eff[0] != 1 and eff[2] != 1:
             sig = (eff[2]-eff[0])/math.sqrt(err[0]**2+err[2]**2+2*sys**2)
-            h_sig.Fill(sig)
             if abs(sig) > 3:
-                print("%i %i %.2f %.2f %.2f %.2f %.2f %.2f %.2f" % (0,2,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig))
+                print("%.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, theta_angle[theta], phi_angle[phi], l_dict[l]))
+            h_sig.Fill(sig)
                 
         if eff[1] != 0 and eff[2] != 0 and eff[1] != 1 and eff[2] != 1:
-            if theta == 7 and phi >= 7 and phi <= 9:
-                sys = sys_value
-            sig = (eff[1]-eff[2])/math.sqrt(err[1]**2+err[2]**2+2*sys**2)
+            sig = (-eff[1]+eff[2])/math.sqrt(err[1]**2+err[2]**2+2*sys**2)
             if abs(sig) > 3:
-                print("%i %i %.2f %.2f %.2f %.2f %.2f %.2f %.2f" % (1,2,eff[0],eff[1],eff[2],a[0],a[1],a[2],sig))
-            else:
-                h_sig.Fill(sig)
+                print("%.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, theta_angle[theta], phi_angle[phi], l_dict[l]))
+            h_sig.Fill(sig)
 
                     
                     
