67a6be1fa259cf13d343d43486c1791afe0128e2
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2016-12-01 17:07:08 +0000

    minor

--- compare.py
+++ compare.py
@@ -44,19 +44,19 @@ h_sig = TH1F("h_sig",";Significance [#sigma];N. Entries / 1",20,-10,10)
 
 def measure_sig(val1,val2):
     return (val1-val2)/math.sqrt(err1**2+err2**2)
-    
+
 theta_angle = {7: 90, 8: 105, 5: 60, 6: 75}
 phi_angle = {7: -90, 8: -75, 9: -60}
 l_dict = {1: 20, 2: 80, 3: 140, 4: 200, 5: 260}
-    
+
 sys = 0
 sys_value = 0.1
 print("0 - Central, 1 - upstream, 2 - downstream")
 
 
 
-with open("output/central_shift.txt") as textfile1, open("output/upstream_shift.txt") as textfile2, open("output/downstream_shift.txt") as textfile3: 
-    for x, y, z, m in zip(textfile1, textfile2, textfile3):
+with open("output/central_shift.txt") as textfile1, open("output/upstream_shift.txt") as textfile2, open("output/downstream_shift.txt") as textfile3:
+    for x, y, z in zip(textfile1, textfile2, textfile3):
         a = x.split(" ")
         a = [float(i) for i in a]
         b = y.split(" ")
@@ -73,24 +73,26 @@ with open("output/central_shift.txt") as textfile1, open("output/upstream_shift.
 
         if eff[0] != 0 and eff[1] != 0 and eff[0] != 1 and eff[1] != 1:
             sig = (-eff[0]+eff[1])/math.sqrt(err[0]**2+err[1]**2+2*sys**2)
-            #if abs(sig) > 3:
+
+            if abs(sig) > 3:
+                print("%.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, theta_angle[theta], phi_angle[phi], l_dict[l]))
             h_sig.Fill(sig)
 
-        
+
         if eff[0] != 0 and eff[2] != 0 and eff[0] != 1 and eff[2] != 1:
             sig = (eff[2]-eff[0])/math.sqrt(err[0]**2+err[2]**2+2*sys**2)
-            #if abs(sig) > 3:
-            #print("%.2f %.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, m[3], theta_angle[theta], phi_angle[phi], l_dict[l]))
-            h_sig.Fill(sig)
-                
-        if eff[1] != 0 and eff[2] != 0 and eff[1] != 1 and eff[2] != 1:
-            sig = (-eff[1]+eff[2])/math.sqrt(err[1]**2+err[2]**2+2*sys**2)
-            #if abs(sig) > 3:
-            #print("%.2f %.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, m[3], theta_angle[theta], phi_angle[phi], l_dict[l]))
+            if abs(sig) > 3:
+                print("%.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, theta_angle[theta], phi_angle[phi], l_dict[l]))
             h_sig.Fill(sig)
 
-                    
-                    
+        if eff[1] != 0 and eff[2] != 0 and eff[1] != 1 and eff[2] != 1:
+            sig = (-eff[1]+eff[2])/math.sqrt(err[1]**2+err[2]**2+2*sys**2)
+            if abs(sig) > 3:
+                print("%.2f %.2f %.2f %.2f %i %i %i" % (float(eff[0]),float(eff[1]),float(eff[2]), sig, theta_angle[theta], phi_angle[phi], l_dict[l]))
+            h_sig.Fill(sig)
+
+
+
 
 c_sig = TCanvas("c_sig")
 h_sig.Draw("ep")
@@ -101,4 +103,4 @@ print(h_sig.Integral())
 #h_sig.GetYaxis().SetRangeUser(0.01,h_sig.GetMaximum()*1.2)
 c_sig.Update()
 
-input()        
+input()
