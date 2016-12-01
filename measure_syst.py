a6f629ecde335857eb846d71242d6854a2cf9263
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2016-12-01 01:15:12 +0000

    2D syst

--- measure_syst.py
+++ measure_syst.py
@@ -59,6 +59,27 @@ with open("output/theta_central_shift.txt") as textfile1, open("output/theta_ups
 
 f_theta.close()
 
+f_phi = open("output/sys_errors_phi.txt","w")
+
+with open("output/phi_central_shift.txt") as textfile1, open("output/phi_upstream_shift.txt") as textfile2, open("output/phi_downstream_shift.txt") as textfile3, open("output/phi_all.txt") as all: 
+    for x, y, z, m in zip(textfile1, textfile2, textfile3, all):
+        a = x.split(" ")
+        a = [float(i) for i in a]
+        b = y.split(" ")
+        b = [float(i) for i in b]
+        c = z.split(" ")
+        c = [float(i) for i in c]
+        m = m.split(" ")
+        m = [float(i) for i in m]
+
+        eff = [a[1],b[1],c[1]]
+        err = [a[2],b[2],c[2]]
+        phi = a[0]
+
+        print("%i %f" % (phi, max(eff)-m[1]), file=f_phi)
+
+f_phi.close()
+
 f_l = open("output/sys_errors_l.txt","w")
 
 with open("output/l_central_shift.txt") as textfile1, open("output/l_upstream_shift.txt") as textfile2, open("output/l_downstream_shift.txt") as textfile3, open("output/l_all.txt") as all: 
@@ -78,3 +99,66 @@ with open("output/l_central_shift.txt") as textfile1, open("output/l_upstream_sh
     	print("%i %f" % (l, max(eff)-m[1]), file=f_l)
 
 f_l.close()
+
+f_theta_phi = open("output/sys_errors_theta_phi.txt","w")
+
+with open("output/theta_phi_central_shift.txt") as textfile1, open("output/theta_phi_upstream_shift.txt") as textfile2, open("output/theta_phi_downstream_shift.txt") as textfile3, open("output/theta_phi_all.txt") as all: 
+    for x, y, z, m in zip(textfile1, textfile2, textfile3, all):
+        a = x.split(" ")
+        a = [float(i) for i in a]
+        b = y.split(" ")
+        b = [float(i) for i in b]
+        c = z.split(" ")
+        c = [float(i) for i in c]
+        m = m.split(" ")
+        m = [float(i) for i in m]
+
+        eff = [a[1],b[1],c[1]]
+        err = [a[2],b[2],c[2]]
+        theta_phi = a[0]
+
+        print("%i %f" % (theta_phi, max(eff)-m[1]), file=f_theta_phi)
+
+f_theta_phi.close()
+
+f_theta_l = open("output/sys_errors_theta_l.txt","w")
+
+with open("output/theta_l_central_shift.txt") as textfile1, open("output/theta_l_upstream_shift.txt") as textfile2, open("output/theta_l_downstream_shift.txt") as textfile3, open("output/theta_l_all.txt") as all: 
+    for x, y, z, m in zip(textfile1, textfile2, textfile3, all):
+        a = x.split(" ")
+        a = [float(i) for i in a]
+        b = y.split(" ")
+        b = [float(i) for i in b]
+        c = z.split(" ")
+        c = [float(i) for i in c]
+        m = m.split(" ")
+        m = [float(i) for i in m]
+
+        eff = [a[1],b[1],c[1]]
+        err = [a[2],b[2],c[2]]
+        theta_l = a[0]
+
+        print("%i %f" % (theta_l, max(eff)-m[1]), file=f_theta_l)
+
+f_theta_l.close()
+
+f_phi_l = open("output/sys_errors_phi_l.txt","w")
+
+with open("output/phi_l_central_shift.txt") as textfile1, open("output/phi_l_upstream_shift.txt") as textfile2, open("output/phi_l_downstream_shift.txt") as textfile3, open("output/phi_l_all.txt") as all: 
+    for x, y, z, m in zip(textfile1, textfile2, textfile3, all):
+        a = x.split(" ")
+        a = [float(i) for i in a]
+        b = y.split(" ")
+        b = [float(i) for i in b]
+        c = z.split(" ")
+        c = [float(i) for i in c]
+        m = m.split(" ")
+        m = [float(i) for i in m]
+
+        eff = [a[1],b[1],c[1]]
+        err = [a[2],b[2],c[2]]
+        phi_l = a[0]
+
+        print("%i %f" % (phi_l, max(eff)-m[1]), file=f_phi_l)
+
+f_phi_l.close()
