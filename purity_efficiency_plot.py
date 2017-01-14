4468e2b750ed225cf565897516017211ecc82204
Stefano Roberto Soleti <stefano.soleti@physics.ox.ac.uk>
2017-01-14 14:47:39 +0000

    new p/a plot

--- purity_efficiency_plot.py
+++ purity_efficiency_plot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.4
 
 from ROOT import TGraphErrors, TCanvas, TLegend, TLine
-from ROOT import kBlue, kRed, kGreen
+from ROOT import kBlue, kRed, kGreen, kGray
 from numpy import array
 import math
 
@@ -16,35 +16,35 @@ tolerance_list = []
 product = []
 product_err = []
 acceptance = []
+data_eff_list = []
+reco_eff_list = []
 f = open("output/purity.txt")
 for line in f:
-    tolerance, eff, eff_err, purity, purity_err = line.split()
+    tolerance, eff, eff_err, purity, purity_err, data_eff = line.split()
     tolerance_list.append(float(tolerance))
     eff_list.append(float(eff)-2.66)
     eff_err_list.append(float(eff_err))
     purity_list.append(float(purity))
     purity_err_list.append(float(purity_err))
     product_value = float(purity)*(float(eff)-2.66)/100
-    product.append(product_value)
-    product_err.append(100*product_error(float(purity)/100,float(eff)/100,float(purity_err)/100,float(eff_err)/100))
     acceptance_value = product_value/(98.96-2.66)
     acceptance.append(acceptance_value*100)
+    data_eff_list.append(float(data_eff))
+    print(float(data_eff)*float(purity)/(acceptance_value*100), tolerance)
+    reco_eff_list.append(float(data_eff)*float(purity)/(acceptance_value*100))
 
 
+reco_eff = array([96.3]*len(eff_list))
 no_err = array([0]*len(eff_list))
+
 c = TCanvas("c")
+g_reco_eff = TGraphErrors(len(eff_list), array(tolerance_list), reco_eff, no_err, array(eff_err_list))
 g_eff = TGraphErrors(len(eff_list), array(tolerance_list), array(eff_list), no_err, array(eff_err_list))
 g_purity = TGraphErrors(len(purity_list), array(tolerance_list), array(purity_list), no_err, array(purity_err_list))
-g_product = TGraphErrors(len(product), array(tolerance_list), array(product), no_err, array(product_err))
-g_acceptance = TGraphErrors(len(acceptance),array(tolerance_list),array(acceptance),no_err, array(product_err))
+g_acceptance = TGraphErrors(len(acceptance),array(tolerance_list),array(acceptance),no_err, array(purity_err_list))
+g_reco_eff_data = TGraphErrors(len(reco_eff_list),array(tolerance_list),array(reco_eff_list),no_err, array(eff_err_list))
+g_data_eff = TGraphErrors(len(reco_eff_list),array(tolerance_list),array(data_eff_list),no_err, array(eff_err_list))
 
-
-#g_product.Draw("APL")
-
-
-g_product.SetMarkerStyle(22)
-g_product.SetLineColor(kGreen+1)
-g_product.SetLineWidth(2)
 g_eff.Draw("APL")
 g_eff.GetXaxis().SetTitle("#it{d}_{max} [cm]")
 g_eff.GetYaxis().SetTitle("[%]")
@@ -60,21 +60,35 @@ g_acceptance.Draw("PL")
 g_acceptance.SetMarkerStyle(23)
 g_acceptance.SetLineColor(kGreen+2)
 g_acceptance.SetLineWidth(2)
-reco_eff_line = TLine(6,98.96-2.66,54,98.96-2.66)
-reco_eff_line.SetLineColor(1)
-reco_eff_line.SetLineStyle(2)
-reco_eff_line.SetLineWidth(2)
-reco_eff_line.Draw()
+g_reco_eff.Draw("PL")
+g_reco_eff.SetLineColor(kGray+2)
+g_reco_eff.SetLineWidth(2)
+g_reco_eff.SetMarkerStyle(29)
+g_reco_eff.SetMarkerSize(1.5)
+g_reco_eff_data.Draw("PL")
+g_data_eff.Draw("PL")
+g_data_eff.SetLineColor(kRed+1)
+g_data_eff.SetLineWidth(2)
+g_data_eff.SetMarkerStyle(4)
+g_data_eff.SetLineStyle(2)
+g_reco_eff_data.SetLineColor(kGray+2)
+g_reco_eff_data.SetLineWidth(2)
+g_reco_eff_data.SetMarkerStyle(30)
+g_reco_eff_data.SetMarkerSize(1.5)
+g_reco_eff_data.SetLineStyle(2)
 
-l = TLegend(0.46,0.16,0.89,0.33)
+l = TLegend(0.46,0.16,0.85,0.425)
 l.SetBorderSize(0)
 l.SetShadowColor(0)
-l.AddEntry(g_eff,"Tagging efficiency #it{#epsilon}_{tag}", "lp")
+
 l.AddEntry(g_purity,"Purity #it{P}", "lp")
-#l.AddEntry(g_product,"#it{#epsilon} x #it{p}", "lp")
 l.AddEntry(g_acceptance,"Acceptance #it{A}", "lp")
 
-l.AddEntry(reco_eff_line,"Reconstruction efficiency #it{#epsilon}", "l")
+l.AddEntry(g_eff,"MC tagging efficiency #it{#epsilon}_{tag}^{MC}", "lp")
+l.AddEntry(g_data_eff,"Data tagging efficiency #it{#epsilon}_{tag}^{data}", "lp")
+
+l.AddEntry(g_reco_eff,"MC reconstruction efficiency #it{#epsilon}_{MC}", "lp")
+l.AddEntry(g_reco_eff_data,"Data reconstruction efficiency #it{#epsilon}_{data}", "lp")
 
 l.Draw()
 
